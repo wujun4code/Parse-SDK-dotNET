1f6be6d3e27d883d0051ae131c98aaa94b7d8e98
WuJun <believeme@outlook.com>
2016-12-28 12:56:12 +0800

    chore: rename the unity init

--- LeanCloud.Core/Public/Unity/AVInitializeBehaviour.cs
+++ LeanCloud.Core/Public/Unity/AVInitializeBehaviour.cs
@@ -7,60 +7,70 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-namespace LeanCloud {
-  /// <summary>
-  /// Mandatory MonoBehaviour for scenes that use LeanCloud. Set the application ID and .NET key
-  /// in the editor.
-  /// </summary>
-  // TODO (hallucinogen): somehow because of Push, we need this class to be added in a GameObject
-  // called `AVInitializeBehaviour`. We might want to fix this.
-  public class AVInitializeBehaviour : MonoBehaviour {
-    private static bool isInitialized = false;
-
+namespace LeanCloud
+{
     /// <summary>
-    /// The LeanCloud applicationId used in this app. You can get this value from the LeanCloud website.
+    /// Mandatory MonoBehaviour for scenes that use LeanCloud. Set the application ID and .NET key
+    /// in the editor.
     /// </summary>
-    [SerializeField]
-    public string applicationID;
+    // TODO (hallucinogen): somehow because of Push, we need this class to be added in a GameObject
+    // called `AVInitializeBehaviour`. We might want to fix this.
+    public class AVInitializeBehaviour : MonoBehaviour
+    {
+        private static bool isInitialized = false;
 
-    /// <summary>
-    /// The LeanCloud dotnetKey used in this app. You can get this value from the LeanCloud website.
-    /// </summary>
-    [SerializeField]
-    public string dotnetKey;
+        /// <summary>
+        /// The LeanCloud applicationId used in this app. You can get this value from the LeanCloud website.
+        /// </summary>
+        [SerializeField]
+        public string applicationID;
 
-    [SerializeField]
-    public string server;
+        /// <summary>
+        /// The LeanCloud applicationKey used in this app. You can get this value from the LeanCloud website.
+        /// </summary>
+        [SerializeField]
+        public string applicationKey;
 
-    /// <summary>
-    /// Initializes the LeanCloud SDK and begins running network requests created by LeanCloud.
-    /// </summary>
-    public virtual void Awake() {
-      Initialize();
+        [SerializeField]
+        public string server;
 
-      // Force the name to be `AVInitializeBehaviour` in runtime.
-      gameObject.name = "AVInitializeBehaviour";
+        [SerializeField]
+        public AVClient.Configuration.AVRegion region;
+
+        /// <summary>
+        /// Initializes the LeanCloud SDK and begins running network requests created by LeanCloud.
+        /// </summary>
+        public virtual void Awake()
+        {
+            Initialize();
+
+            // Force the name to be `AVInitializeBehaviour` in runtime.
+            gameObject.name = "AVInitializeBehaviour";
+        }
+
+        public void Initialize()
+        {
+            if (isInitialized)
+            {
+                return;
+            }
+
+            isInitialized = true;
+            // Keep this gameObject around, even when the scene changes.
+            GameObject.DontDestroyOnLoad(gameObject);
+
+            AVClient.Initialize(new AVClient.Configuration
+            {
+                ApplicationId = applicationID,
+                ApplicationKey = applicationKey,
+                Server = string.IsNullOrEmpty(server) ? null : server,
+                Region = region
+            });
+
+            Dispatcher.Instance.GameObject = gameObject;
+
+            // Kick off the dispatcher.
+            StartCoroutine(Dispatcher.Instance.DispatcherCoroutine);
+        }
     }
-
-    public void Initialize() {
-      if (isInitialized) {
-        return;
-      }
-
-      isInitialized = true;
-      // Keep this gameObject around, even when the scene changes.
-      GameObject.DontDestroyOnLoad(gameObject);
-
-      AVClient.Initialize(new AVClient.Configuration {
-        ApplicationId = applicationID,
-        ApplicationKey = dotnetKey,
-        Server = string.IsNullOrEmpty(server) ? null : server
-      });
-
-      Dispatcher.Instance.GameObject = gameObject;
-
-      // Kick off the dispatcher.
-      StartCoroutine(Dispatcher.Instance.DispatcherCoroutine);
-    }
-  }
 }
