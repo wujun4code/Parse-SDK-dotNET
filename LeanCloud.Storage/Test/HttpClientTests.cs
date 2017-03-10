88e2ec59342e61b51f7cb3ae5eeeed45a140444b
Ang Long <aisk1988@gmail.com>
2017-03-10 18:08:36 +0800

    test: add user agent test

---
+++ LeanCloud.Storage/Test/HttpClientTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using LeanCloud.Storage.Internal;
+
+namespace ParseTest
+{
+    class HttpClientTests
+    {
+        [Test]
+        public Task TestUserAgent()
+        {
+            HttpRequest request = new HttpRequest();
+            request.Uri = new Uri("http://httpbin.org/user-agent");
+            request.Method = "GET";
+            HttpClient client = new HttpClient();
+            return client.ExecuteAsync(request, null, null, new CancellationToken()).ContinueWith(t => {
+                Assert.IsFalse(t.IsFaulted);
+                Assert.IsFalse(t.IsCanceled);
+                string body = t.Result.Item2;
+                Assert.IsTrue(body.Contains("LeanCloud-dotNet-SDK/"));
+            });
+        }
+    }
+}
